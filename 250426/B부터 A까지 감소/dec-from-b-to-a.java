bcebcbe1ffaf7e439d09eb3428c17856e15d021c
gyulmung <mseunghyeon1222@gmail.com>
2025-04-26 15:40:16 +0900

    Title: B부터 A까지 감소, Time: 100ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250426/B부터 A까지 감소/dec-from-b-to-a.java
@@ -0,0 +1,16 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        // Please write your code here.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+
+        int a = Integer.parseInt(arr[0]);
+        int b = Integer.parseInt(arr[1]);
+        
+        for(int i = b; i >= a; i--){
+            System.out.printf("%d ",i);
+        }
+    }
+}
