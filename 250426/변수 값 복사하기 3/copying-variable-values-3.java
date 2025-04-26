17fa382dd20af368f35378bb224f987e8875cc1f
gyulmung <mseunghyeon1222@gmail.com>
2025-04-26 13:10:54 +0900

    Title: 변수 값 복사하기 3, Time: 93ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250426/변수 값 복사하기 3/copying-variable-values-3.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 1;
+        int b = 5;
+        int c = 3;
+        a = c;
+        a = a+c;
+        b = b-c;
+        System.out.printf("%d\n%d\n%d", a, b, c);    
+    }
+}
