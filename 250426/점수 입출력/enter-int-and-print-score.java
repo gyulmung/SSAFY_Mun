982a5ea8bf87b2ab4422b0163ef40c78ac61fbd7
gyulmung <mseunghyeon1222@gmail.com>
2025-04-26 13:29:41 +0900

    Title: 점수 입출력, Time: 122ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250426/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println("Your score is "+ a + " point.");
+        sc.close();
+    }
+}
