a79f26c47c64fe997a92b2e03e2dc1410bf2445c
gyulmung <mseunghyeon1222@gmail.com>
2025-04-26 13:13:01 +0900

    Title: 사칙연산 연습, Time: 99ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250426/사칙연산 연습/practice-basic-arithmetic-operations.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5;
+        int b = 30;
+        int c = a + b;
+        int c_value = c / 8;
+        int c_under = c % 8;
+
+        System.out.printf("%d\n%d", c_value, c_under);
+    }
+}
