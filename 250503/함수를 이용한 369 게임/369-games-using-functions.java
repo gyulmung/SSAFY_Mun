934dd88380e4373b0037e2fabb9c1bc2c19142bb
gyulmung <mseunghyeon1222@gmail.com>
2025-05-03 14:29:44 +0900

    Title: 함수를 이용한 369 게임, Time: 105ms, Memory: 9MB, Status: Wrong Answer - Codetree

--- 250503/함수를 이용한 369 게임/369-games-using-functions.java
+++ 250503/함수를 이용한 369 게임/369-games-using-functions.java
@@ -4,7 +4,7 @@ public class Main {
     public static int func(int a, int b){
         int cnt = 0;
         for(int i = a ; i <= b ; i++ ){
-            if (i % 3 == 0 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9 || i / 10 == 3 || i / 10 == 6 | i / 10 == 9){
+            if (i % 3 == 0 || i % 10 == 3 || i % 10 == 6 || i % 10 == 9 || i / 10 == 3 || i / 10 == 6 || i / 10 == 9){
                 cnt += 1;
             }
         }
