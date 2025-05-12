a9093bc86dffd30b5d83d78f99063fb232e6f57b
gyulmung <mseunghyeon1222@gmail.com>
2025-05-12 23:21:50 +0900

    Title: 사칙연산 함수, Time: 91ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250512/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,27 @@
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] arr = br.readLine().split(" ");
+        int a = Integer.parseInt(arr[0]);
+        char b = arr[1].charAt(0);
+        int c = Integer.parseInt(arr[2]);
+        // Please write your code here.
+
+        if (b == '+'){
+            System.out.printf("%d + %d = %d",a, c, a+c);
+        }
+        else if (b == '-'){
+            System.out.printf("%d - %d = %d",a, c, a-c);
+        }
+        else if (b == '*'){
+            System.out.printf("%d * %d = %d",a, c, a*c);
+        }
+        else if(b == '/'){
+            System.out.printf("%d / %d = %d",a, c, a+c);
+        }
+        else{
+            System.out.printf("False");
+        }
+    }
+}
